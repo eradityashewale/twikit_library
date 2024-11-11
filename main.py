9414c9c43b4ed10bd46f0e3e9f40a62427862aad
Aditya Nathu Shewale <141516636+eradityashewale@users.noreply.github.com>
2024-11-11 16:16:00 +0530

    Added function to save cookies

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+from twikit import Client, TooManyRequests
+import time
+from datetime import datetime
+import csv
+from configparser import ConfigParser
+from random import randint
+
+MINIMUM_TWEETS = 10
+QUERY = '$ETH'
+
+config = ConfigParser()
+config.read('config.ini')
+username = config["X"]['username']
+password = config["X"]['password']
+email = config["X"]['email']
+
+async def main():
+    client = Client(language='en-US')
+
+    # Await login since it's an async function
+    await client.login(
+        auth_info_1=username,
+        auth_info_2=email,
+        password=password
+    )
+    # Save cookies after logging in
+    client.save_cookies('cookies.json')
+
+# Run the async main function
+asyncio.run(main())
